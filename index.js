cf9fbbeca8c075cfbcadb0d162f9bd14c50a5d5c
Sarah Stephens <sarahstephens84@gmail.com>
2022-09-22 16:57:30 -0400

    commit tree 4d65f44f1f6082add62027a623267e77761b98b2

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distanceInFeet){
   // Write your code here!
+  if (distanceInFeet < 400){
+  return 'This one is on me!';
+} else if (distanceInFeet < 2000 && distanceInFeet > 400){
+  return 'That will be twenty bucks.';
+} else if (distanceInFeet < 2500 && distanceInFeet > 2000){
+  return 'I will gladly take your thirty bucks.';
+}else if (distanceInFeet > 2500) {
+  return 'No can do.';
+}
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  }  
+    else{
+      return "No go.";
+    }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+    return "Thank you so much."
+    break;
+    case 'not as generous':
+    return "Thank you."
+    break;
+    default:
+    return "Bye."
+  }
+
 }
